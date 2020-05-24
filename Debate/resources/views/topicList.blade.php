898d9e18811366d66ea9b25c7dabcbb6c59aa19e
mk1018 <ktmn1018@yahoo.co.jp>
2020-05-24 17:03:31 +0900

    微修正

--- Debate/resources/views/topicList.blade.php
+++ Debate/resources/views/topicList.blade.php
@@ -4,19 +4,19 @@
     <div class="container">
         <div class="row justify-content-left">
                 @foreach ($topics as $topic)
-                <form method="GET" action="discuss" class="form-inline m-1">
                 <div class="col-md-4 mb-2">
                     <div class="card">
                         <div class="card-header">
-                            <input type="hidden" name="topic_no" value="{{ $topic->topic_no }}">
-                            <button type="submit" class="btn btn-primary col-md-6">部屋へ移動する</button>
+                            <form method="GET" action="discuss" class="form-inline m-1">
+                                <input type="hidden" name="topic_no" value="{{ $topic->topic_no }}">
+                                <button type="submit" class="btn btn-primary col-md-6">部屋へ移動する</button>
+                            </form>
                         </div>
                         <div class="card-body">
                             {{ $topic->content }}
                         </div>
                     </div>
                 </div>
-                </form>
                 @endforeach
         </div>
     </div>
